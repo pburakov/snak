25a6c380616c03ae238a5984d1ca30e58277ba94
Paul Burakov <pburakov@gmail.com>
2019-07-03 09:03:24 +0100

    initial commit

---
+++ Assets/Snake.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    Vector2 direction = Vector2.right;
+    List<Transform> tail = new List<Transform>();
+    bool ate = false;
+    public GameObject tailPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Move", 0.15f, 0.15f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            direction = Vector2.right;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            direction = -Vector2.up; // '-up' means 'down'
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            direction = -Vector2.right; // '-right' means 'left'
+        else if (Input.GetKey(KeyCode.UpArrow))
+            direction = Vector2.up;
+    }
+
+    void Move()
+    {
+        Vector2 currentPos = transform.position;
+        // Move head one pixel into the direction
+        // (will create a gap between the head and the first element of the tail)
+        transform.Translate(direction);
+
+        if (ate)
+        {
+            GameObject tailPiece = (GameObject)Instantiate(tailPrefab, currentPos, Quaternion.identity);
+            tail.Insert(0, tailPiece.transform);
+            ate = false;
+        }
+        else
+        if (tail.Count > 0) // is there a tail?
+        {
+            // Move last element to a gap between the head
+            // and the first element of the tail
+            tail.Last().position = currentPos;
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count - 1);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name.StartsWith("FoodPrefab"))
+        {
+            ate = true;
+            Destroy(collision.gameObject);
+        }
+        else // collided with tail or border
+        {
+            direction = Vector2.zero;
+        }
+    }
+}
---
+++ Assets/SpawnFood.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SpawnFood : MonoBehaviour
+{
+    public GameObject foodPrefab;
+
+    public Transform borderTop;
+    public Transform borderBottom;
+    public Transform borderLeft;
+    public Transform borderRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 3, 4);
+    }
+
+    void Spawn()
+    {
+        int x = (int)Random.Range(borderLeft.position.x, borderRight.position.x);
+        int y = (int)Random.Range(borderTop.position.y, borderBottom.position.y);
+        Instantiate(foodPrefab, new Vector2(x, y), Quaternion.identity);
+    }
+}
